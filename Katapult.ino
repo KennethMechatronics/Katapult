c7ef6bd0c549e5efa3e42d871ba5381cd0ddbc28
KennethMechatronics <118351204+KennethMechatronics@users.noreply.github.com>
2023-03-26 11:03:25 +0200

    Update Katapult.ino

--- Katapult.ino
+++ Katapult.ino
@@ -1,50 +1,52 @@
 #include <Servo.h>
 #include <LiquidCrystal.h>
 
-const int step = 13;
-const int dir = 12;
-const int ms1 = 11;
+//                rs en d4 d5 d6 d7
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
-const int echoPin = 9;
+Servo servo;
+
 const int trigPin = 8;
+const int step = 10;
+const int ms1 = 11;
+const int dir = 12;
 
-const int knappLast = A3;
-const int knappKast = A4;
-const int sjokksensor = A5;
+const int sjokksensor = 13;
+const int knappLast = A0;   // 14
+const int knappKast = A1;   // 15
+const int endebryter = A2;  // 16
 
-const int endebryter = A2;
+const int potKastStep = A3;  // 17
+const int servoPot = A4;     // 18
+const int echoPin = A5;      // 19
 
-const int servoPot = A0;
-const int potKastStep = A1;
+int servoPos;
+const int servoNullpunkt = 100;
 
-const float graderPrStep = 1.8;
+
 float antStep;
 int iAntStep;
 int utgangsvinkel;
-const int delayKast = 1000;  // 800 er en bra verdi.
+float buelengde;  // [m]
+const float graderPrStep = 1.8;
+const float pi = 3.1415;
+const float lengdeKasteArm = 0.150;  // [m]
+const int delayKast = 900;
 
-bool harKastet = false;
-bool traff = false;
+float distanse = 0.0;
 
 unsigned long t0;
 unsigned long t1;
 unsigned long t2;
-float distanse = 0;
-
-const int timeout = 5000;
-
-const float lengdeKasteArm = 0.147;  //[m]
-const float pi = 3.1415;
-float buelengde = 0.0;
-const float mBadeand = 0.005;  // [kg]
 
 unsigned long fortid;
-int intervall = 500;
+const int intervall = 500;
+const int timeout = 5000;
 
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
-//                rs en d4 d5 d6 d7
+bool harKastet = false;
+bool traff = false;
 
-byte katapult[] = {
+byte katapult[] = {  // https://maxpromer.github.io/LCD-Character-Creator/
   B00011,
   B00010,
   B00100,
@@ -66,18 +68,6 @@ byte prosjektil[] = {
   B00000
 };
 
-Servo servo;
-int servoPos;
-const int servoNullpunkt = 100;
-
-float tall[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-const String dataNavn[9] = { "Akselerasjonstid", "Total tid", "Flytid", "Akselerasjon", "Kraft", "Utgangshastighet", "Horisontal hastighet", "Utgangshoyde", "Max banehoyde" };
-
-
-
-
-
-
 void setup() {
 
   Serial.begin(9600);
@@ -94,24 +84,19 @@ void setup() {
   lcd.setCursor(13, 1);
   lcd.write(byte(1));
 
-
-
-  for (int i = 11; i <= 13; i++) {
+  for (int i = 8; i <= 12; i++) {
     pinMode(i, OUTPUT);
   }
 
-  servo.attach(10);
+  servo.attach(9);
 
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  for (int j = 13; j <= 16; j++) {
+    pinMode(j, INPUT_PULLUP);
+  }
 
-  pinMode(knappLast, INPUT_PULLUP);
-  pinMode(knappKast, INPUT_PULLUP);
-  pinMode(endebryter, INPUT_PULLUP);
-  pinMode(sjokksensor, INPUT_PULLUP);
-
-  pinMode(servoPot, INPUT);
-  pinMode(potKastStep, INPUT);
+  for (int k = 17; k <= 19; k++) {
+    pinMode(k, INPUT);
+  }
 
   digitalWrite(ms1, LOW);
 
@@ -125,14 +110,31 @@ void setup() {
 void loop() {
   unsigned long naatid = millis();
 
-  int servoPos = map(analogRead(servoPot), 0, 1023, 0, 180);
-  servo.write(servoPos);
+
 
   int utgangsvinkel = map(analogRead(potKastStep), 0, 1023, 0, 100);
   antStep = (utgangsvinkel / graderPrStep);
   iAntStep = antStep;
 
-  float buelengde = 2.0 * pi * lengdeKasteArm * (utgangsvinkel / 360.0);  // [m]
+  if (harKastet == true) {
+    if (digitalRead(sjokksensor) == LOW) {
+      t2 = millis();
+      Serial.println("Treff");
+
+      float buelengde = 2.0 * pi * lengdeKasteArm * (utgangsvinkel / 360.0);  // [m]
+
+      beregninger(t0, t1, t2, buelengde, distanse, utgangsvinkel, lengdeKasteArm, pi);
+
+    } else if (digitalRead(sjokksensor) == HIGH && millis() >= t1 + timeout) {
+      Serial.println("Du bomma");
+      harKastet = !harKastet;
+      delay(1000);
+      lastProsjektil();
+    }
+  }
+
+  int servoPos = map(analogRead(servoPot), 0, 1023, 0, 180);
+  servo.write(servoPos);
 
   if (naatid - fortid >= intervall) {
     fortid = naatid;
@@ -153,33 +155,19 @@ void loop() {
     Serial.println("Kaster");
     delay(50);
     kast();
-    delay(50);
   }
 
   if (digitalRead(knappLast) == LOW) {
     lastProsjektil();
   }
-
-  if (harKastet == true) {
-    if (digitalRead(sjokksensor) == LOW) {
-      t2 = millis();
-      Serial.println("Treff");
-
-      beregninger(t0, t1, t2, buelengde, distanse, utgangsvinkel, mBadeand, lengdeKasteArm, pi);
-
-    } else if (digitalRead(sjokksensor) == HIGH && millis() >= t1 + timeout) {
-      Serial.println("Du bomma");
-      harKastet = !harKastet;
-      delay(1000);
-      lastProsjektil();
-    }
-  }
 }
 
 
 void kast() {
+
   digitalWrite(dir, LOW);
   digitalWrite(ms1, LOW);
+  Serial.println(String(iAntStep) + " step");
 
   t0 = millis();
 
@@ -194,6 +182,7 @@ void kast() {
 
   harKastet = true;
 }
+
 
 void lastProsjektil() {
   Serial.println("Kjorer til endeposisjon");  // Kunne ikke ha denne inne i for-loopen pga. treghet og støy.
@@ -210,7 +199,7 @@ void lastProsjektil() {
   digitalWrite(ms1, HIGH);  // senker til HALF STEP
 
   while (digitalRead(endebryter) == HIGH) {
-    for (int j = 0; j < 10; j++) {
+    for (int l = 0; l < 10; l++) {
       digitalWrite(step, HIGH);
       delayMicroseconds(1500);
       digitalWrite(step, LOW);
@@ -224,11 +213,12 @@ void lastProsjektil() {
       lcd.print("Endeposisjon");
       lcd.setCursor(0, 1);
       lcd.print("naad");
-      delay(1000);
+      delay(700);
       return;
     }
   }
 }
+
 
 float hentDistanse() {
 
@@ -245,14 +235,41 @@ float hentDistanse() {
   return distanse;  // returnerer distanse [m]
 }
 
-float beregninger(long t0, long t1, long t2, float buelengde, float distanse, float utgangsvinkel, float mBadeand, float lengdeKasteArm, float pi) {
+
+void beregninger(long t0, long t1, long t2, float buelengde, float distanse, int utgangsvinkel, float lengdeKasteArm, float pi) {
+  const float mBadeand = 5.0;  // [g]
+
   Serial.println("Starter beregninger...");
+  Serial.println("Data inn til funksjonen: \n");
+  Serial.println("t0         \t" + String(t0));
+  Serial.println("t1         \t" + String(t1));
+  Serial.println("t2         \t" + String(t2));
+  Serial.println("Buelengde  \t" + String(buelengde));
+  Serial.println("Distanse   \t" + String(distanse));
+  Serial.println("Utg.vink   \t" + String(utgangsvinkel));
+  Serial.println("mBadeand   \t" + String(mBadeand));
+  Serial.println("lengdeKArm \t" + String(lengdeKasteArm));
+  Serial.println("pi         \t" + String(pi) + "\n");
+
+
+  float tall[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+  const String dataNavn[9] = { 
+    "Akselerasjonstid    "
+  , "Total tid           "
+  , "Flytid              "
+  , "Akselerasjon        "
+  , "Kraft               "
+  , "Utgangshastighet    "
+  , "Horisontal hastighet"
+  , "Utgangshoyde        "
+  , "Max banehoyde       " };
+
 
   float aksTid = (t1 - t0) / 1000.0;                                                                             // [s]
   float totTid = (t2 - t0) / 1000.0;                                                                             // [s]
   float flytid = (t2 - t1) / 1000.0;                                                                             // [s]
-  float akselerasjon = (2.0 * ((buelengde) - (aksTid))) / pow(aksTid, 2);                                        // [m/s^2]
-  float Fkast = mBadeand * akselerasjon;                                                                         // [N]
+  float akselerasjon = (2.0 * (buelengde - aksTid)) / pow(aksTid, 2);                                            // [m/s^2]
+  float Fkast = (mBadeand / 1000) * akselerasjon;                                                                // [N]
   float utgangshastighet = akselerasjon * (aksTid);                                                              // [m/s]
   float horisontalHastighet = distanse / (flytid);                                                               // [m/s]
   float utgangshoyde = lengdeKasteArm * sin(utgangsvinkel * (pi / 180)) + 0.1;                                   // [m] over bakken    (0.1 er høyde til omdreiningspunkt)
@@ -268,39 +285,45 @@ float beregninger(long t0, long t1, long t2, float buelengde, float distanse, fl
   tall[7] = utgangshoyde;
   tall[8] = maxBanehoyde;
 
-  for (int i = 0; i <= 8; i++) {
-    Serial.print(dataNavn[i] + "\t" + tall[i]);
+  for (int m = 0; m <= 8; m++) {
+    Serial.print(dataNavn[m] + "  " + tall[m]);
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print(dataNavn[i]);
+    lcd.print(dataNavn[m]);
     lcd.setCursor(0, 1);
-    lcd.print(tall[i]);
-    switch (i) {
+    lcd.print(tall[m]);
+    switch (m) {
       case 0 ... 2:
         Serial.print(" [s]");
-        lcd.print("         [s]");
+        lcd.setCursor(13, 1);
+        lcd.print("[s]");
         break;
       case 3:
-        Serial.print(" [m/s^2]");
-        lcd.print("    [m/s^2]");
+        Serial.print("[m/s^2]");
+        lcd.setCursor(9, 1);
+        lcd.print("[m/s^2]");
         break;
       case 4:
         Serial.print(" [N]");
-        lcd.print("         [N]");
+        lcd.setCursor(13, 1);
+        lcd.print("[N]");
         break;
       case 5 ... 6:
         Serial.print(" [m/s]");
-        lcd.print("       [m/s]");
+        lcd.setCursor(11, 1);
+        lcd.print("[m/s]");
         break;
       case 7 ... 8:
         Serial.print(" [m]");
-        lcd.print("         [m]");
+        lcd.setCursor(13, 1);
+        lcd.print("[m]");
         break;
     }
     Serial.println();
-    delay(3000);
+    delay(2000);
   }
   lcd.clear();
+
   harKastet = !harKastet;
 }
 
